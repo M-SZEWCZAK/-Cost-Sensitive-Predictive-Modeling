5b4fb0608af510b9851802d798c978a871b44090
M-Szewczak <miszewc4@gmail.com>
2026-05-23 20:59:52 +0200

    best threshold returned by scoring function

--- eda/scoring_function.py
+++ eda/scoring_function.py
@@ -1,6 +1,4 @@
-import pandas as pd
 import numpy as np
-from pandas.conftest import ascending
 
 
 def score_model_optimal_k(y_true, y_proba, n_vars, max_k=1000, keep_fp_tp=False,tp_reward=10,fp_penalty=5,feature_penalty=200):
@@ -16,6 +14,7 @@ def score_model_optimal_k(y_true, y_proba, n_vars, max_k=1000, keep_fp_tp=False,
     fp = 0
     best_fp=0
     best_tp=0
+    best_threshold=0
     for k in range(1, min(max_k, len(y_true)) + 1):
         if y_true_sorted[k-1] == 1:
             tp += 1
@@ -30,7 +29,6 @@ def score_model_optimal_k(y_true, y_proba, n_vars, max_k=1000, keep_fp_tp=False,
             best_fp = fp
             best_tp = tp
             best_threshold = y_proba_sorted[k - 1]
-    print("Best threshold:", best_threshold)
     if keep_fp_tp:
-        return best_score, best_k, best_fp, best_tp
-    return best_score, best_k
+        return best_score,best_threshold, best_k, best_fp, best_tp
+    return best_score,best_threshold, best_k
